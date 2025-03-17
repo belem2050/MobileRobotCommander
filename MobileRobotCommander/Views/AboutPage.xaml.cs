be571a087f75885e84930a05009d4006952652e9
Moumouni BELEM <moumouni.belem@novalynx.fr>
2025-03-17 23:33:51 +0100

    feat: add empty pages

---
+++ MobileRobotCommander/Views/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MobileRobotCommander.Views;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MobileRobotCommander/Views/SettingsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MobileRobotCommander.Views;
+
+public partial class SettingsPage : ContentPage
+{
+	public SettingsPage()
+	{
+		InitializeComponent();
+	}
+}
