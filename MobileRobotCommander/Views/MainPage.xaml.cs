c88af2d1a21c2820db3a04a442bb1fded6daab3f
Moumouni BELEM <moumouni.belem@novalynx.fr>
2025-03-17 23:34:52 +0100

    feta: move MianPage to Views folder and add first main view display

---
+++ MobileRobotCommander/Views/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+
+namespace MobileRobotCommander.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //private void OnConnectClicked(object sender, EventArgs e)
+        //{
+        //    CommandLog.Text += $"\nConnecting to {IpAddressEntry.Text}...";
+        //}
+
+        //private void OnMoveClicked(object sender, EventArgs e)
+        //{
+        //    if (sender is Button btn)
+        //    {
+        //        CommandLog.Text += $"\nMoving: {btn.Text}";
+        //    }
+        //}
+
+        //private void OnRotateClicked(object sender, EventArgs e)
+        //{
+        //    if (sender is Button btn)
+        //    {
+        //        CommandLog.Text += $"\nRotating: {btn.Text}";
+        //    }
+        //}
+
+        //private void OnVoiceCommand(object sender, EventArgs e)
+        //{
+        //    CommandLog.Text += "\nVoice Command Activated...";
+        //}
+
+        //private void OnStopClicked(object sender, EventArgs e)
+        //{
+        //    CommandLog.Text += "\nSTOP!";
+        //}
+    }
+}
