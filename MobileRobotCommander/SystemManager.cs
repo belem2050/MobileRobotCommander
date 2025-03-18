5c23007555940bec3983d6816f88fd59c029153d
Moumouni BELEM <moumouni.belem@novalynx.fr>
2025-03-18 23:41:22 +0100

    refactor: add systemManager

---
+++ MobileRobotCommander/SystemManager.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MobileRobotCommander.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileRobotCommander
+{
+    public sealed class SystemManager : ObservableObject
+    {
+        private static object _lockInstance = new object();
+        static private SystemManager? _instance = null;
+
+        public SettingsPageVm Settings { get; private set; } = new SettingsPageVm();
+
+        private SystemManager()
+        {
+            _instance = this;
+        }
+
+        static public SystemManager GetInstance()
+        {
+            lock (_lockInstance)
+            {
+                if (_instance is null)
+                {
+                    return _instance = new SystemManager();
+                }
+                return _instance;
+            }
+        }
+    }
+}
+
+
