7abdb56fafe5c8169dd5e0aa928272fc283ca9e8
Moumouni BELEM <moumouni.belem@novalynx.fr>
2025-03-18 23:40:55 +0100

    refactor: add settings and about vm

---
+++ MobileRobotCommander/ViewModels/AboutPageVm.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MobileRobotCommander.ViewModels
+{
+    public partial class AboutPageVm : ObservableObject
+    {
+    }
+}
--- MobileRobotCommander/ViewModels/SettingsPageVm.cs
+++ MobileRobotCommander/ViewModels/SettingsPageVm.cs
@@ -2,7 +2,30 @@
 
 namespace MobileRobotCommander.ViewModels
 {
-    public partial class Settings : ObservableObject
+
+
+
+    public partial class SettingsPageVm : ObservableObject
     {
+        [ObservableProperty]
+        private string cmdVelocityCommandTopic = "/cmd_vel";
+
+        [ObservableProperty]
+        private int port = 9090;
+        [ObservableProperty]
+        private double maxLinearSpeed = 1.0;
+
+        [ObservableProperty]
+        private double minLinearSpeed = -1.0;
+
+        [ObservableProperty]
+        private double maxAngularSpeed = 1.0;
+
+        [ObservableProperty]
+        private double minAngularSpeed = -1.0;
+
+
+        public SettingsPageVm() 
+        { } 
     }
 }
