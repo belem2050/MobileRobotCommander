fa2a28138158ed8abfa8f401b0134931af6ced19
Moumouni BELEM <moumouni.belem@novalynx.fr>
2025-03-22 22:05:06 +0100

    feat: add grammar for keywords recognition

---
+++ MobileRobotCommander/Data/Grammar.cs
@@ -0,0 +1,61 @@
+﻿namespace MobileRobotCommander.Data
+{
+    public class Grammar
+    {
+        public static readonly List<string> Forward = 
+        [
+            "forward",
+            "go",
+            "move forward",
+            "ko straight",
+            "keep going"
+        ];
+
+        public static readonly List<string> Backward =
+        [
+            "backward",
+            "go back",
+            "move backward",
+            "go straight",
+            "keep going"
+        ];
+
+        public static readonly List<string> Stop =
+        [
+            "stop",
+            "don't move",
+        ];
+
+        public static readonly List<string> RotateLeft =
+        [
+            "rotate left",
+        ];
+
+        public static readonly List<string> RotateRight =
+        [
+            "rotate right",
+        ];
+
+        public static readonly List<string> TurnLeft =
+        [
+            "turn left",
+        ];
+
+        public static readonly List<string> TurnRight =
+        [
+            "turn left",
+        ];
+
+        public static readonly List<string> BackwardLeft =
+        [
+              "go back left",
+              "back left",
+        ];
+
+        public static readonly List<string> BackwardRight =
+        [
+              "go back right",
+              "back right",
+        ];
+    }
+}
