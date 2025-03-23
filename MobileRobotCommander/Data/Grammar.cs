adac623f3128eac36e770f45ff27e39b6ae7d79d
Moumouni BELEM <moumouni.belem@novalynx.fr>
2025-03-23 01:46:41 +0100

    refactor: add more grammar

--- MobileRobotCommander/Data/Grammar.cs
+++ MobileRobotCommander/Data/Grammar.cs
@@ -2,13 +2,16 @@
 {
     public class Grammar
     {
-        public static readonly List<string> Forward = 
+        public static readonly List<string> Forward =
         [
             "forward",
             "go",
             "move forward",
-            "ko straight",
-            "keep going"
+            "go straight",
+            "keep going",
+            "move ahead",
+            "proceed",
+            "advance"
         ];
 
         public static readonly List<string> Backward =
@@ -16,46 +19,73 @@
             "backward",
             "go back",
             "move backward",
-            "go straight",
-            "keep going"
+            "reverse",
+            "back up",
+            "step back"
         ];
 
         public static readonly List<string> Stop =
         [
             "stop",
             "don't move",
+            "halt",
+            "freeze",
+            "hold position",
+            "stay put"
         ];
 
         public static readonly List<string> RotateLeft =
         [
             "rotate left",
+            "turn counterclockwise",
+            "spin left",
+            "pivot left",
+            "rotate to the left"
         ];
 
         public static readonly List<string> RotateRight =
         [
             "rotate right",
+            "turn clockwise",
+            "spin right",
+            "pivot right",
+            "rotate to the right"
         ];
 
         public static readonly List<string> TurnLeft =
         [
             "turn left",
+            "go left",
+            "steer left",
+            "veer left",
+            "move left"
         ];
 
         public static readonly List<string> TurnRight =
         [
-            "turn left",
+            "turn right",
+            "go right",
+            "steer right",
+            "veer right",
+            "move right"
         ];
 
         public static readonly List<string> BackwardLeft =
         [
-              "go back left",
-              "back left",
+            "go back left",
+            "back left",
+            "reverse left",
+            "move backward left",
+            "step back left"
         ];
 
         public static readonly List<string> BackwardRight =
         [
-              "go back right",
-              "back right",
+            "go back right",
+            "back right",
+            "reverse right",
+            "move backward right",
+            "step back right"
         ];
     }
 }
